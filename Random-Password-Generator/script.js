9a3bf71549132fce21f821b97bfa0324a8ba7ddf
Simret <simretabera7@gmail.com>
2026-05-06 10:14:23 +0300

    adding comments on the HTML file rebuilding the css  and starting the script.js file

---
+++ Random-Password-Generator/script.js
@@ -0,0 +1,17 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("click", () => {
+  createPassword();
+});
+
+function createPassword() {
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const passwordLength = 14;
+  let password = "";
+
+  for (let i = 0; i < passwordLength; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+}
